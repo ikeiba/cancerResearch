0b8066f359f31d3303696c60d3c3e3154773c0cd
ikeiba <ibarrola.iker@opendeusto.es>
2025-02-28 13:32:57 +0100

    Update descriptive_analysis_breast_cancer.R

--- descriptive_analysis_breast_cancer.R
+++ descriptive_analysis_breast_cancer.R
@@ -1,6 +1,29 @@
+library(stringr)
 
-data <- read.csv("./breast_cancer_updated.csv")
+data <- read.csv("./data.csv")
 head(data)
 str(data)
 dim(data)
-sort(data$radius_mean)[4000:5000]
+sort(data$radius_mean)
+
+
+# Ensure that there are no incorrect observations in the dataset (meaning that a variable is = 0 when it could not)
+zero_removal <- function(data){
+  for (column in names(data)) {
+    amount <- sum(data[[column]] == 0, na.rm = TRUE)
+    
+    if (amount > 0){
+      message <- stringr::str_glue("The column {column} had {amount} elements that were equal to 0")
+      print(message)
+      
+      data[[column]][data[[column]] == 0] <- NA
+    }
+  }
+  return(data)
+}
+
+# Assign the result to the data variable (update data variable)
+data <- zero_removal(data)
+
+
+
