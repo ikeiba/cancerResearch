906d0eb38c83c97d674fd2532728727650dd9a57
CarlosFirvida <carlos.firvida@opendeusto.es>
2025-03-31 16:26:49 +0200

    Correction - Logistic Regression (Prostate Cancer)

--- logistic_regression_prostate_cancer.R
+++ logistic_regression_prostate_cancer.R
@@ -1,4 +1,4 @@
-# LINEAR REGRESSION MODEL OF THE WISCONSIN BREAST CANCER DATASET 
+# LOGISTIC REGRESSION MODEL OF THE PROSTATE CANCER DATASET 
 # Authors: Iker Ibarrola, Enetz Quindimil, Carlos Firvida, Iñigo Infante
 
 
@@ -8,11 +8,14 @@ library(pastecs)
 library(ggplot2)
 library(lattice)
 library(car)
+library(caret)
+library(pROC)
+library(ResourceSelection) # for the Hosmer-Lemeshow test
 
 
-###################################
-# DATASET LOADING & CHECKING
-###################################
+##############################
+# DATASET LOADING & CHECKING #
+##############################
 
 data <- read.csv("./Prostate_Cancer.csv") # We read the dataset from the csv file
 
@@ -20,9 +23,10 @@ head(data) # We visualize the first six rows of the dataset
 
 str(data) # We visualize the structure of the data (the different variables, their type and some initial values)
 
-###################################
-# DATASET MODIFICATION 
-###################################
+
+########################
+# DATASET MODIFICATION #
+########################
 
 # We make some of the modificatons previously done in the descriptive analysis part:
 
@@ -32,12 +36,14 @@ data$diagnosis_result <- factor(data$diagnosis_result) # Create a factor from th
 levels(data$diagnosis_result) <- c("benign", "malign") # Instead of using 'b' & 'm', use more descriptive labels
 summary(data$diagnosis_result) # We check that previously steps worked properly
 
-###################################
-# RESPONSE SELECTION
-###################################
+
+######################
+# RESPONSE SELECTION #
+######################
 
 # We have selected the Diagnosis Result as the response, being the reason that it is the only
 # categorycal variable that we have in this dataset.
+
 
 ######################################
 # SOME PLOTS FOR PREDICTOR SELECTION #
@@ -45,8 +51,9 @@ summary(data$diagnosis_result) # We check that previously steps worked properly
 
 # Let´s take a look in the prediction vs the other parameters
 
-# Create violin plots using ggplot
-# Diagnosis Result vs Radius: it is not a good predictor because os the similarity of the distribution betweenn bening and maling
+# CREATE VIOLIN PLOTS USING GGPLOT
+
+# Diagnosis Result vs Radius: it is not a good predictor because os the similarity of the distribution between bening and maling
 ggplot(data, aes(x = diagnosis_result, y = radius, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Radius",
@@ -55,7 +62,7 @@ ggplot(data, aes(x = diagnosis_result, y = radius, fill = diagnosis_result)) +
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
   theme_minimal() 
 
-# Diagnosis Result vs
+# Diagnosis Result vs Texture: it is also not a good predictor due to the similar distribution in both bening and malign
 ggplot(data, aes(x = diagnosis_result, y = texture, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Texture",
@@ -64,7 +71,7 @@ ggplot(data, aes(x = diagnosis_result, y = texture, fill = diagnosis_result)) +
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
   theme_minimal() 
 
-# Diagnosis Result vs this good?
+# Diagnosis Result vs Perimeter: this is a good predictor, we can see the difference in the distibution of the results on the diagnosis result
 ggplot(data, aes(x = diagnosis_result, y = perimeter, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Perimeter",
@@ -73,15 +80,16 @@ ggplot(data, aes(x = diagnosis_result, y = perimeter, fill = diagnosis_result)) 
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
   theme_minimal() 
 
-# Diagnosis Result vs this good?
+# Diagnosis Result vs Area: as expected due to the relation with the perimeter value, this is a good predictor
 ggplot(data, aes(x = diagnosis_result, y = area, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Area",
        title = "Violin Plot: Area vs. Diagnosis Result") +
   geom_boxplot(width=0.25, fill = NA, color = "black")+
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
-  theme_minimal() 
-# Diagnosis Result vs
+  theme_minimal()
+
+# Diagnosis Result vs Smoothness: this is not a good predictor
 ggplot(data, aes(x = diagnosis_result, y = smoothness, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Smoothness",
@@ -90,7 +98,7 @@ ggplot(data, aes(x = diagnosis_result, y = smoothness, fill = diagnosis_result))
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
   theme_minimal() 
 
-# Diagnosis Result vs this good?
+# Diagnosis Result vs Compactness: this is a good predictor as we can see in the distribution showed in the violin plot
 ggplot(data, aes(x = diagnosis_result, y = compactness, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Compactness",
@@ -99,7 +107,7 @@ ggplot(data, aes(x = diagnosis_result, y = compactness, fill = diagnosis_result)
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
   theme_minimal() 
 
-# Diagnosis Result vs
+# Diagnosis Result vs Symmetry: symmetry is not a good predictor
 ggplot(data, aes(x = diagnosis_result, y = symmetry, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Symmetry",
@@ -108,7 +116,7 @@ ggplot(data, aes(x = diagnosis_result, y = symmetry, fill = diagnosis_result)) +
   scale_fill_manual(values = c("brown1", "cornflowerblue")) +
   theme_minimal() 
 
-# Diagnosis Result vs
+# Diagnosis Result vs Fractalm Dimension: fractal dimension is neither a good predictor
 ggplot(data, aes(x = diagnosis_result, y = fractal_dimension, fill = diagnosis_result)) +
   geom_violin(trim=FALSE)+
   labs(x = "Diagnosis Result", y = "Fractal Dimension",
@@ -118,11 +126,16 @@ ggplot(data, aes(x = diagnosis_result, y = fractal_dimension, fill = diagnosis_r
   theme_minimal() 
 
 
+#############################
+# PLOT WITH SOME PREDICTORS #
+#############################
+
 # Now let's plot the probability of diagnosis_result vs some of the predictors 
 # and fit it using a LR and a logistic regression model
 # Add the probability of :
 data$prob_diagnosis_result <- ifelse(data$diagnosis_result == "benign", 1, 0)
 
+# - PERIMETER - #
 
 # To start we are going to use as the predictor the perimeter: as we checked in the PREDICTOR SELECTION,
 # the perimeter is a good predictor and we can see it in this scatter plot thanks to the curve of the
@@ -137,7 +150,7 @@ data$predicted_probs <- predict(logit_model, type = "response")
 
 # Create scatter plot with logistic regression curve
 Plot_PerimVsDef_LogFit <- 
-  ggplot(data, aes(x = perimeter, y = prob_default)) +
+  ggplot(data, aes(x = perimeter, y = prob_diagnosis_result)) +
   geom_point(aes(color = "Observations")) +  # Scatter plot
   geom_line(aes(y = predicted_probs, color = "Logistic Model")) +  # Log regression
   scale_color_manual(values = c("Observations" = "cornflowerblue", 
@@ -149,9 +162,11 @@ Plot_PerimVsDef_LogFit <-
        color = "Legend") +
   theme_minimal()
 
+# - RADIUS - #
 
 # Now let´s try with the radius: we are seeing in the graph that as we notice in the PREDICTOR SELECTION
-# radius is not a good predictor, now we can see it because of the straight line that does not fit the data
+# radius is not a good predictor, now we can see it because of the straight line that generates ought to
+# the even distribution of the results
 
 # Create scatter plot with a logistic linear regression fit
 # Fit a logistic regression model
@@ -162,7 +177,7 @@ data$predicted_probs <- predict(logit_model, type = "response")
 
 # Create scatter plot with logistic regression curve
 Plot_RadiusVsDef_LogFit <- 
-  ggplot(data, aes(x = radius, y = prob_default)) +
+  ggplot(data, aes(x = radius, y = prob_diagnosis_result)) +
   geom_point(aes(color = "Observations")) +  # Scatter plot
   geom_line(aes(y = predicted_probs, color = "Logistic Model")) +  # Log regression
   scale_color_manual(values = c("Observations" = "cornflowerblue", 
@@ -170,12 +185,13 @@ Plot_RadiusVsDef_LogFit <-
                      breaks = c("Observations", 
                                 "Logistic Model")) +  # order legend items
   labs(x = "Radius", y = "",
-       title = "Scatter Plot: Radius vs. Perimeter",
+       title = "Scatter Plot: Diagnosis Result vs. Radius",
        color = "Legend") +
   theme_minimal()
 
 
-# We will check one last time with the compactness which should give us a good result: [ASK Giulio]
+# We will check one last time with the compactness which should give us a good result: as we predicted,
+# the compactness is a good predictor
 
 # Create scatter plot with a logistic linear regression fit
 # Fit a logistic regression model
@@ -185,8 +201,8 @@ logit_model <- glm(prob_diagnosis_result ~ compactness, data = data, family = bi
 data$predicted_probs <- predict(logit_model, type = "response")
 
 # Create scatter plot with logistic regression curve
-Plot_CompactVsDef_LogFit <- 
-  ggplot(data, aes(x = radius, y = prob_default)) +
+Plot_CompactnessVsDef_LogFit <- 
+  ggplot(data, aes(x = compactness, y = prob_diagnosis_result)) +
   geom_point(aes(color = "Observations")) +  # Scatter plot
   geom_line(aes(y = predicted_probs, color = "Logistic Model")) +  # Log regression
   scale_color_manual(values = c("Observations" = "cornflowerblue", 
@@ -194,9 +210,82 @@ Plot_CompactVsDef_LogFit <-
                      breaks = c("Observations", 
                                 "Logistic Model")) +  # order legend items
   labs(x = "Compactness", y = "",
-       title = "Scatter Plot: Compactness vs. Perimeter",
+       title = "Scatter Plot: Diagnosis Result vs. Compactness",
        color = "Legend") +
   theme_minimal()
+
+
+#######################
+# LOGISTIC REGRESSION #
+#######################
+
+# Model syntax #
+# We are now ready to fit our model
+mod0 <- glm(diagnosis_result ~ texture+perimeter+smoothness+compactness+symmetry+fractal_dimension,
+            family=binomial(),
+            data=data)
+summary(mod0)
+
+# Now we create a new model without the symmetry and check again
+mod1 <- glm(diagnosis_result ~ texture+perimeter+smoothness+compactness+fractal_dimension,
+            family=binomial(),
+            data=data)
+summary(mod1)
+
+# Now we create a new model without the smoothness and check again
+mod2 <- glm(diagnosis_result ~ texture+perimeter+compactness+fractal_dimension,
+            family=binomial(),
+            data=data)
+summary(mod2)
+
+# Now we create a new model without the perimeter and check again
+mod3 <- glm(diagnosis_result ~ texture+compactness+fractal_dimension,
+            family=binomial(),
+            data=data)
+summary(mod3)
+
+# Finally, we create a new model without the perimeter and check again
+mod4 <- glm(diagnosis_result ~ compactness+fractal_dimension,
+            family=binomial(),
+            data=data)
+summary(mod4)
+
+
 #########################
-# CLASSIFICATION TABLES # [ASK Giulio]
+# CLASSIFICATION TABLES #
 #########################
+
+# The table is produced with two variables: data of the response and predictions
+# The response is:
+response <- data$prob_diagnosis_result
+
+# In order to compute our predictions we need to fix the cut-off point
+# Let's select cp=0.5
+cp <- 0.5
+
+# Let us now use the cut-off point to obtain our predictions
+prediction <- ifelse(mod4$fitted.values < cp,0,1) 
+
+# We can now create the table
+tab <- confusionMatrix(as.factor(prediction), as.factor(response), positive="1")
+tab
+
+# Have a look at the table and compute the accuracy, sensitivity and specificity
+
+ac <- (tab$table[1]+tab$table[4])/nrow(data)
+sn <- tab$table[4]/(tab$table[3]+tab$table[4])
+sp <- tab$table[1]/(tab$table[1]+tab$table[2])
+c(ac,sn,sp)
+
+# ROC
+# The ROC curve is plot of the sensitivity as a function of 1-specificity
+par(pty="s")
+Roc <- roc(response,mod4$fitted.values, plot=TRUE, legacy.axes=TRUE, print.auc=TRUE)
+
+# OPTIMAL cut-off POINT
+# The optimal cut-off point can be obtained from the point the sensitivity and the specificity curves cross
+plot(Roc$thresholds,Roc$sensitivities,type="l",xlab="cut-off points",ylab="Sensitivity/Specificity",bty="n")
+lines(Roc$thresholds,Roc$specificities)
+text(x=0.7,y=0.05,labels = "Sensitivity")
+text(x=0.7,y=0.95,labels = "Specificity")
+abline(v=0.084,col="gray",lty=2)
