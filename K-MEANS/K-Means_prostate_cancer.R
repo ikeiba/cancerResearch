b8d812cd0871176823b2f5ab922346703c941a5c
CarlosFirvida <carlos.firvida@opendeusto.es>
2025-05-14 09:19:41 +0200

    Update K-Means_prostate_cancer.R

--- K-MEANS/K-Means_prostate_cancer.R
+++ K-MEANS/K-Means_prostate_cancer.R
@@ -22,9 +22,22 @@ data <- read.csv("C:/Users/carlos.firvida/OneDrive - Universidad de Deusto/Docum
 
 # Remove non-numeric or ID variables
 data <- data %>% select(-id)
-data_pca <- data[, sapply(data, is.numeric)] # Only the numeric data for the PCA
-data_pca <- scale(data_pca) # PCA requires standardization if variables are on different scales
 
-# View the structure of the dataset
+## PCA ANALYSIS
+data <- data[, sapply(data, is.numeric)] # Only the numeric data for the PCA
+data_pca <- scale(data) # PCA requires standardization if variables are on different scales
+# Perform Principal Component Analysis
+pca_result <- PCA(data_pca)
+plot.PCA(pca_result, choix = "var", axes = c(2, 3))
+
+# We visualize the summary, to check every section of PCA
+summary(pca_result)
+pca_result$eig    # Only eigenvalues of components 1 and 2 are above 1 representing
+                # 63% of the data, but component 3 is near with 0.98 so we will take it
+                # into account representing 75% of the total data
+
+pca_data <- pca_result$ind$coord[,1:3] # This stores the dataset after applying pca
+
+# View the 6 first lines of each dataset
 head(data)
-head(data_pca)
+head(pca_data)
